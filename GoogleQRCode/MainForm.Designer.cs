b248b88b2b07751f4a03d64fcff950d3631bba8e
Kevin Huang <Kevin-JH.Huang@sgs.com>
2017-05-24 13:51:35 +0800

    Initialized.

---
+++ GoogleQRCode/MainForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace GoogleQRCode
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.p1 = new System.Windows.Forms.Panel();
+			this.txtContent = new System.Windows.Forms.TextBox();
+			this.orpic = new System.Windows.Forms.PictureBox();
+			this.p2 = new System.Windows.Forms.Panel();
+			this.picOpenQr = new System.Windows.Forms.PictureBox();
+			this.txtDencoder = new System.Windows.Forms.TextBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.exit = new System.Windows.Forms.PictureBox();
+			this.print = new System.Windows.Forms.PictureBox();
+			this.decod = new System.Windows.Forms.PictureBox();
+			this.save = new System.Windows.Forms.PictureBox();
+			this.open = new System.Windows.Forms.PictureBox();
+			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.timeshowlable = new System.Windows.Forms.ToolStripStatusLabel();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.printDocument = new System.Drawing.Printing.PrintDocument();
+			this.skinEngine1 = new Sunisoft.IrisSkin.SkinEngine();
+			this.p1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.orpic)).BeginInit();
+			this.p2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picOpenQr)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.exit)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.print)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.decod)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.save)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.open)).BeginInit();
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// p1
+			// 
+			this.p1.Controls.Add(this.txtContent);
+			this.p1.Controls.Add(this.orpic);
+			this.p1.Location = new System.Drawing.Point(1, 59);
+			this.p1.Name = "p1";
+			this.p1.Size = new System.Drawing.Size(312, 342);
+			this.p1.TabIndex = 1;
+			// 
+			// txtContent
+			// 
+			this.txtContent.Location = new System.Drawing.Point(3, 3);
+			this.txtContent.Multiline = true;
+			this.txtContent.Name = "txtContent";
+			this.txtContent.Size = new System.Drawing.Size(306, 124);
+			this.txtContent.TabIndex = 1;
+			this.txtContent.TextChanged += new System.EventHandler(this.txtContent_TextChanged);
+			// 
+			// orpic
+			// 
+			this.orpic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.orpic.Location = new System.Drawing.Point(50, 133);
+			this.orpic.Name = "orpic";
+			this.orpic.Size = new System.Drawing.Size(262, 193);
+			this.orpic.TabIndex = 0;
+			this.orpic.TabStop = false;
+			// 
+			// p2
+			// 
+			this.p2.Controls.Add(this.picOpenQr);
+			this.p2.Controls.Add(this.txtDencoder);
+			this.p2.Location = new System.Drawing.Point(2, 57);
+			this.p2.Name = "p2";
+			this.p2.Size = new System.Drawing.Size(312, 342);
+			this.p2.TabIndex = 2;
+			this.p2.Visible = false;
+			// 
+			// picOpenQr
+			// 
+			this.picOpenQr.Location = new System.Drawing.Point(35, 15);
+			this.picOpenQr.Name = "picOpenQr";
+			this.picOpenQr.Size = new System.Drawing.Size(216, 192);
+			this.picOpenQr.TabIndex = 1;
+			this.picOpenQr.TabStop = false;
+			// 
+			// txtDencoder
+			// 
+			this.txtDencoder.BackColor = System.Drawing.Color.White;
+			this.txtDencoder.Location = new System.Drawing.Point(3, 213);
+			this.txtDencoder.Multiline = true;
+			this.txtDencoder.Name = "txtDencoder";
+			this.txtDencoder.Size = new System.Drawing.Size(303, 121);
+			this.txtDencoder.TabIndex = 0;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.exit);
+			this.groupBox1.Controls.Add(this.print);
+			this.groupBox1.Controls.Add(this.decod);
+			this.groupBox1.Controls.Add(this.save);
+			this.groupBox1.Controls.Add(this.open);
+			this.groupBox1.Location = new System.Drawing.Point(1, -1);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(312, 54);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			// 
+			// exit
+			// 
+			this.exit.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.exit.Image = global::GoogleQRCode.Properties.Resources.exit;
+			this.exit.Location = new System.Drawing.Point(257, 9);
+			this.exit.Name = "exit";
+			this.exit.Size = new System.Drawing.Size(42, 41);
+			this.exit.TabIndex = 4;
+			this.exit.TabStop = false;
+			this.exit.Click += new System.EventHandler(this.exit_Click);
+			this.exit.MouseLeave += new System.EventHandler(this.exit_MouseLeave);
+			this.exit.MouseMove += new System.Windows.Forms.MouseEventHandler(this.exit_MouseMove);
+			// 
+			// print
+			// 
+			this.print.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.print.Image = ((System.Drawing.Image)(resources.GetObject("print.Image")));
+			this.print.Location = new System.Drawing.Point(199, 9);
+			this.print.Name = "print";
+			this.print.Size = new System.Drawing.Size(42, 41);
+			this.print.TabIndex = 3;
+			this.print.TabStop = false;
+			this.print.Click += new System.EventHandler(this.print_Click);
+			this.print.MouseLeave += new System.EventHandler(this.print_MouseLeave);
+			this.print.MouseMove += new System.Windows.Forms.MouseEventHandler(this.print_MouseMove);
+			// 
+			// decod
+			// 
+			this.decod.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.decod.Image = global::GoogleQRCode.Properties.Resources.encoding;
+			this.decod.Location = new System.Drawing.Point(136, 9);
+			this.decod.Name = "decod";
+			this.decod.Size = new System.Drawing.Size(42, 41);
+			this.decod.TabIndex = 2;
+			this.decod.TabStop = false;
+			this.decod.Click += new System.EventHandler(this.decod_Click);
+			this.decod.MouseLeave += new System.EventHandler(this.decod_MouseLeave);
+			this.decod.MouseMove += new System.Windows.Forms.MouseEventHandler(this.decod_MouseMove);
+			// 
+			// save
+			// 
+			this.save.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.save.Image = ((System.Drawing.Image)(resources.GetObject("save.Image")));
+			this.save.Location = new System.Drawing.Point(73, 10);
+			this.save.Name = "save";
+			this.save.Size = new System.Drawing.Size(42, 41);
+			this.save.TabIndex = 1;
+			this.save.TabStop = false;
+			this.save.Click += new System.EventHandler(this.save_Click);
+			this.save.MouseLeave += new System.EventHandler(this.save_MouseLeave);
+			this.save.MouseMove += new System.Windows.Forms.MouseEventHandler(this.save_MouseMove);
+			// 
+			// open
+			// 
+			this.open.AccessibleDescription = "";
+			this.open.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.open.Image = ((System.Drawing.Image)(resources.GetObject("open.Image")));
+			this.open.Location = new System.Drawing.Point(12, 11);
+			this.open.Name = "open";
+			this.open.Size = new System.Drawing.Size(42, 41);
+			this.open.TabIndex = 0;
+			this.open.TabStop = false;
+			this.open.Tag = "打开";
+			this.open.Click += new System.EventHandler(this.open_Click);
+			this.open.MouseLeave += new System.EventHandler(this.open_MouseLeave);
+			this.open.MouseMove += new System.Windows.Forms.MouseEventHandler(this.open_MouseMove);
+			// 
+			// timer
+			// 
+			this.timer.Enabled = true;
+			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.timeshowlable});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 404);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(314, 22);
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(299, 17);
+			this.toolStripStatusLabel1.Spring = true;
+			this.toolStripStatusLabel1.Text = "当前时间：";
+			this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// timeshowlable
+			// 
+			this.timeshowlable.Name = "timeshowlable";
+			this.timeshowlable.Size = new System.Drawing.Size(0, 17);
+			// 
+			// printDocument
+			// 
+			this.printDocument.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument_PrintPage);
+			// 
+			// skinEngine1
+			// 
+			this.skinEngine1.@__DrawButtonFocusRectangle = true;
+			this.skinEngine1.DisabledButtonTextColor = System.Drawing.Color.Gray;
+			this.skinEngine1.DisabledMenuFontColor = System.Drawing.SystemColors.GrayText;
+			this.skinEngine1.InactiveCaptionColor = System.Drawing.SystemColors.InactiveCaptionText;
+			this.skinEngine1.SerialNumber = "";
+			this.skinEngine1.SkinFile = null;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(314, 426);
+			this.Controls.Add(this.p2);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.p1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "QRCode";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.p1.ResumeLayout(false);
+			this.p1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.orpic)).EndInit();
+			this.p2.ResumeLayout(false);
+			this.p2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.picOpenQr)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.exit)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.print)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.decod)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.save)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.open)).EndInit();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Panel p1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.TextBox txtContent;
+		private System.Windows.Forms.PictureBox orpic;
+
+		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.PictureBox open;
+		private System.Windows.Forms.PictureBox save;
+		private System.Windows.Forms.PictureBox exit;
+		private System.Windows.Forms.PictureBox print;
+		private System.Windows.Forms.PictureBox decod;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ToolStripStatusLabel timeshowlable;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Panel p2;
+		private System.Windows.Forms.PictureBox picOpenQr;
+		private System.Windows.Forms.TextBox txtDencoder;
+		private System.Drawing.Printing.PrintDocument printDocument;
+		private Sunisoft.IrisSkin.SkinEngine skinEngine1;
+
+	}
+}
+
---
+++ GoogleQRCode/MainForm.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using com.google.zxing;
+using ByteMatrix = com.google.zxing.common.ByteMatrix;
+using EAN13Writer = com.google.zxing.oned.EAN13Writer;
+using EAN8Writer = com.google.zxing.oned.EAN8Writer;
+using MultiFormatWriter = com.google.zxing.MultiFormatWriter;
+using System.IO;
+
+namespace GoogleQRCode
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			this.skinEngine1.SkinFile = Application.StartupPath + "//skins//SportsGreen.ssk";
+			this.p1.Visible = true;
+			//this.p2.Visible = false;
+
+			//用于实现PictureBox的鼠标移动过去的提示
+			ToolTip toolTip = new ToolTip();
+			toolTip.AutoPopDelay = 5000;//一下4个都是属性
+			toolTip.InitialDelay = 1000;
+			toolTip.ReshowDelay = 500;
+			toolTip.ShowAlways = true;
+			toolTip.SetToolTip(this.open, "打开");//参数1是button名,参数2是要显示的内容
+			toolTip.SetToolTip(this.save, "保存");
+			toolTip.SetToolTip(this.print,"打印二维码");
+			toolTip.SetToolTip(this.decod,"解码");
+			toolTip.SetToolTip(this.exit,"返回生成二维码");
+		}
+
+
+		private void txtContent_TextChanged(object sender, EventArgs e)
+		{
+			string content = txtContent.Text;
+			if (content == "")
+			{
+				MessageBox.Show("请输入内容", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			ByteMatrix byteMatrix = new MultiFormatWriter().encode(content, BarcodeFormat.QR_CODE, 200, 200);
+			Bitmap bitmap = toBitmap(byteMatrix);
+			orpic.Image = bitmap;
+			
+		}
+
+		public static Bitmap toBitmap(ByteMatrix matrix)
+		{
+			int width = matrix.Width;
+			int height = matrix.Height;
+			Bitmap bmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					bmap.SetPixel(x, y, matrix.get_Renamed(x, y) != -1 ? ColorTranslator.FromHtml("0xFF000000") : ColorTranslator.FromHtml("0xFFFFFFFF"));
+				}
+			}
+			return bmap;
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			this.timeshowlable.Text = DateTime.Now.ToString();
+		}
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void exit_MouseMove(object sender, MouseEventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath+@"\image\exit-1.png");
+			this.exit.Image = map;
+		}
+
+		private void exit_MouseLeave(object sender, EventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\exit.png");
+			this.exit.Image = map;
+		}
+
+		private void print_MouseMove(object sender, MouseEventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\print-1.png");
+			this.print.Image = map;
+		}
+
+		private void print_MouseLeave(object sender, EventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\print.png");
+			this.print.Image = map;
+		}
+
+		private void open_MouseLeave(object sender, EventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\open.png");
+			this.open.Image = map;
+		}
+
+		private void open_MouseMove(object sender, MouseEventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\open-1.png");
+			this.open.Image = map;
+		}
+
+		private void save_MouseLeave(object sender, EventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\save.png");
+			this.save.Image = map;
+		}
+
+		private void save_MouseMove(object sender, MouseEventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\save-1.png");
+			this.save.Image = map;
+		}
+
+		private void decod_MouseMove(object sender, MouseEventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\encoding-1.png");
+			this.decod.Image = map;
+		}
+
+		private void decod_MouseLeave(object sender, EventArgs e)
+		{
+			System.Drawing.Bitmap map = new Bitmap(Application.StartupPath + @"\image\encoding.png");
+			this.decod.Image = map;
+		}
+
+		private void open_Click(object sender, EventArgs e)
+		{
+			if (p1.Visible == true)
+			{
+				if (this.openFileDialog1.ShowDialog() != DialogResult.OK)
+				{
+					return;
+				}
+				string path = this.openFileDialog1.FileName;
+				openFileDialog1.DefaultExt = "*.txt";
+				openFileDialog1.Filter = "*.txt|";
+				if (path.EndsWith(".txt"))
+				{
+					string content = File.ReadAllText(path, Encoding.Default);
+					this.txtContent.Text = content;
+				}
+				else
+				{
+					MessageBox.Show("只能读取txt文件","提示",MessageBoxButtons.OK,MessageBoxIcon.Error);
+				}
+			}
+			else
+			{
+				if (this.openFileDialog1.ShowDialog() != DialogResult.OK)
+				{
+					return;
+				}
+				string path = this.openFileDialog1.FileName;
+				if (!path.EndsWith(".png"))
+				{
+					MessageBox.Show("图像格式不正确，只能读取png文件", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+				else
+				{
+					Image img = Image.FromFile(path);
+					Bitmap bmap;
+					try
+					{
+						bmap = new Bitmap(img);
+					}
+					catch (System.IO.IOException ioe)
+					{
+						MessageBox.Show(ioe.ToString());
+						return;
+					}
+					if (bmap == null)
+					{
+						MessageBox.Show("Could not decode image");
+						return;
+					}
+					else
+					{
+						this.picOpenQr.Image = bmap;
+					}
+					LuminanceSource source = new RGBLuminanceSource(bmap, bmap.Width, bmap.Height);
+					com.google.zxing.BinaryBitmap bitmap = new com.google.zxing.BinaryBitmap(new com.google.zxing.common.HybridBinarizer(source));
+					Result result;
+					try
+					{
+						result = new MultiFormatReader().decode(bitmap);
+					}
+					catch (ReaderException re)
+					{
+						MessageBox.Show(re.ToString());
+						return;
+					}
+
+					this.txtDencoder.Text = (result.Text);
+				}
+			}
+		}
+
+		private void save_Click(object sender, EventArgs e)
+		{
+			if (p1.Visible == true)
+			{
+				saveFileDialog1.Filter = "*.png|";
+				//saveFileDialog1.DefaultExt = "*.png";
+				saveFileDialog1.RestoreDirectory = true;
+				if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+				{
+					string localPath = saveFileDialog1.FileName.ToString();
+					Image qrcode = orpic.Image;
+					if (qrcode == null)
+					{
+						MessageBox.Show("无保存项", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+					}
+					else
+					{
+						if (localPath.EndsWith(".png"))
+						{
+							qrcode.Save(localPath);
+						}
+						else
+						{
+							qrcode.Save(localPath + ".png");
+						}
+
+					}
+				}
+			}
+			else
+			{
+				saveFileDialog1.Filter = "*.txt|";
+				//saveFileDialog1.DefaultExt = "*.txt";
+				if(this.saveFileDialog1.ShowDialog()==DialogResult.OK)
+				{
+					string localPath = saveFileDialog1.FileName.ToString()+".txt";
+					if (File.Exists(localPath))
+					{
+						DialogResult RESULT = MessageBox.Show("是否确认覆盖原有文件?", "信息提示", MessageBoxButtons.YesNo);
+						if (RESULT.ToString().Equals("Yes"))
+						{
+							File.Delete(localPath);
+							File.AppendAllText(localPath, this.txtDencoder.Text, Encoding.Default);
+						}
+						else
+						{
+							return;
+						}
+					}
+					else
+					{
+						File.AppendAllText(localPath, this.txtDencoder.Text, Encoding.Default);
+					}
+				}
+			}
+		}
+
+		private void exit_Click(object sender, EventArgs e)
+		{
+			this.p2.Visible = false;
+			this.p1.Visible=true;
+		}
+
+		private void decod_Click(object sender, EventArgs e)
+		{
+			this.p2.Visible=true;
+			this.p1.Visible = !(p1.Visible);
+		}
+
+		private void print_Click(object sender, EventArgs e)
+		{
+			PrintDialog printDialog = new PrintDialog();
+			printDialog.Document = printDocument;
+			if (printDialog.ShowDialog() == DialogResult.OK) { try
+                {
+                    printDocument.Print();
+                }
+                catch
+                {   //停止打印
+                    printDocument.PrintController.OnEndPrint(printDocument, new System.Drawing.Printing.PrintEventArgs());
+                }
+            }
+        }
+
+		private void printDocument_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+		{
+			if (this.p1.Visible == true)
+			{
+				e.Graphics.DrawImage(this.orpic.Image, 20, 20);
+			}
+			else
+			{
+				e.Graphics.DrawImage(this.picOpenQr.Image, 20, 20);
+			}
+		}
+	}
+}
